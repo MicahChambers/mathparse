9d162464272c97d7f26c06eafd885b0db63ec272
Micah Chambers (scrapper) <micahcc@ucla.edu>
2014-09-28 16:26:40 -0700

    nearly done, need to reorganize code though

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
+all: mathparse regex_test
 
 mathparse: mathparse.cpp
-	clang++ mathparse.cpp -o mathparse -std=c++11 -ggdb
+	g++ -Wall mathparse.cpp -o mathparse -std=c++11 -ggdb
+
+regex_test: regex_test.cpp
+	g++ -Wall regex_test.cpp -o regex_test -std=c++11 -ggdb
 
 clean:
 	rm mathparse
--- mathparse.cpp
+++ mathparse.cpp
@@ -1,4 +1,3 @@
-#include <regex>
 #include <string>
 #include <iostream>
 #include <unordered_map>
@@ -10,6 +9,7 @@
 #include <algorithm>
 #include <cassert>
 #include <random>
+#include <memory>
 
 #include <stdexcept>
 
@@ -35,17 +35,11 @@ private:
     function<double()> executor;
 };
 
-void explainRegexError(std::regex_error& e);
 void printInfix(std::list<string> rpn);
+list<string> tokenize(string exp);
+list<string> infixreorder(list<string> exp);
 
-//class UnaryMap : unordered_map<string, function<double(double)>> {
-//    UnaryMap() 
-//    {
-//        pointer_to_unary_function<double,double> f(exp);
-//        (*this)["exp"] = f;
-//    }
-//} UNARY;
-unordered_map<string,int> priority({{"exp", 5}, {"cos", 5}, {"sin", 5},
+unordered_map<string,int> PRIORITY({{"exp", 5}, {"cos", 5}, {"sin", 5},
         {"tan", 5}, {"log", 5}, {"+", 3}, {"-",3}, {"*", 4},
         {"/", 4}, {"==", 2}, {"^", 6}});
 
@@ -73,47 +67,32 @@ int main(int argc, char** argv)
         cerr << "Need 1 argument: an equation" << endl;
         return -1;
     }
-
-    string eq(argv[1]);
-    cerr << "Equation: " << eq << endl;
-    std::string ops;
-    for(auto it = UNARY.begin(); it != UNARY.end(); ++it) 
-        ops += "|" + it->first;
-    for(auto it = BINARY.begin(); it != BINARY.end(); ++it) {
-        if(it->first == "*" || it->first == "+" || it->first == "^")
-            ops += "|\\" + it->first;
-        else
-            ops += "|" + it->first;
+    auto tokens = tokenize(argv[1]);
+    auto rpn = infixreorder(tokens);
+    cerr << "RPN:";
+    for(auto it = rpn.begin(); it != rpn.end(); it++) {
+        cerr << " " << *it;;
     }
+    cerr << endl;
+    printInfix(rpn);
+    Equation func(rpn);
+}
 
-    regex re;
-    try {
-        string regexstr = "\\s*(\\(|\\)"+ops+"|[0-9]*|[a-z]*)\\s*";
-//        string regexstr = "\\s*(\\(|\\)|log|cos|sin|exp|tan|-|==|\\+|[0-9]*|[a-z]*)";
-        cerr << "RE:\n" << regexstr << endl;
-        re.assign(regexstr);
-    } catch(regex_error& e) {
-        explainRegexError(e);
-        return -1;
-    }
-    regex_iterator<string::iterator> rit (eq.begin(), eq.end(), re,
-            regex_constants::match_continuous);
-    regex_iterator<string::iterator> rend;
-
-    unordered_map<string, double> args;
+list<string> infixreorder(list<string> tokens) 
+{
     list<string> opstack;
     list<string> outqueue;
     string prev = "";
     bool impliedmult = false;
     bool prevarg = false;
-    for(; rit != rend && !rit->str().empty(); ++rit) {
-        string tok = (*rit)[1];
-
+    for(auto it=tokens.begin(); it != tokens.end(); ++it) {
+        string tok = *it;
+        cerr << tok << endl;
         if(impliedmult && tok != ")" && (tok == "(" || BINARY.count(tok) == 0)) {
             while(!opstack.empty()) {
-                // Go ahead and evaluate higher priority operators before 
+                // Go ahead and evaluate higher PRIORITY operators before 
                 // the current
-                if(priority["*"] <= priority[opstack.front()]) {
+                if(PRIORITY["*"] <= PRIORITY[opstack.front()]) {
                     outqueue.push_back(opstack.front());
                     opstack.pop_front();
                 } else {
@@ -141,19 +120,19 @@ int main(int argc, char** argv)
             opstack.pop_front();
             impliedmult = true;
             prevarg = true;
-        } else if(priority.count(tok) > 0) {
+        } else if(PRIORITY.count(tok) > 0) {
             // check for prefix +-
             if(!prevarg && tok == "+") {
                 // ignore + prefix 
             } else if(!prevarg && tok == "-") {
-                // prefix negate has highest priority
+                // prefix negate has highest PRIORITY
                 opstack.push_front("neg");
             } else {
-                // Add latest operator to stack until we find lower priority op
+                // Add latest operator to stack until we find lower PRIORITY op
                 while(!opstack.empty()) {
-                    // Go ahead and evaluate higher priority operators before 
+                    // Go ahead and evaluate higher PRIORITY operators before 
                     // the current
-                    if(priority[tok] <= priority[opstack.front()]) {
+                    if(PRIORITY[tok] <= PRIORITY[opstack.front()]) {
                         outqueue.push_back(opstack.front());
                         opstack.pop_front();
                     } else {
@@ -172,22 +151,16 @@ int main(int argc, char** argv)
             prevarg = true;
         }
     }
-	
+
     // Copy last operators to output queue
-	while(!opstack.empty()) {
-		if(opstack.front() == "(") 
+    while(!opstack.empty()) {
+        if(opstack.front() == "(") 
             throw INVALID_ARGUMENT("Error, unmatched parentheses remaining");
-		outqueue.push_back(opstack.front());
-		opstack.pop_front();
-	}
-	
-    cerr << "RPN:";
-    for(auto it = outqueue.begin(); it != outqueue.end(); it++) {
-        cerr << " " << *it;;
+        outqueue.push_back(opstack.front());
+        opstack.pop_front();
     }
-    cerr << endl;
-    printInfix(outqueue);
-    Equation func(outqueue);
+
+    return outqueue;
 }
 
 void printInfix(std::list<string> rpn)
@@ -221,42 +194,11 @@ void printInfix(std::list<string> rpn)
     cerr << "INF:" << stack.back() << endl;
 }
 
-string randstr() 
-{
-    static const char alphanum[] =
-        "0123456789"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-
-    const int LEN = 32;
-    std::uniform_int_distribution<int> dist(0, sizeof(alphanum)-1);
-
-    string out(LEN, 'a');
-    out[0] = 'k';
-    for (int i = 1; i < out.size(); ++i) {
-        out[i] = alphanum[dist(rng)];
-    }
-    return out;
-}
-
-bool isnumeric(string str)
-{
-    regex re("[+-]?\\d*(?:.\\d*)?(?:e[+-]?\\d*)?(?:.\\d*)?");
-    cerr << "[+-]?\\d*(?:.\\d*)?(?:e[+-]?\\d*)?(?:.\\d*)?" << endl;
-    cerr << str << endl;
-    if(regex_match(str.begin(), str.end(), re)) {
-        cerr << "matches" << endl;
-        return true;
-    }
-    return false;
-}
-
 Equation::Equation(std::list<string> rpn)
 {
 	using namespace std::placeholders;
     
     args.clear();
-    char* endptr;
     function<double()> lhs;
     function<double()> rhs;
     pair<unordered_map<string,shared_ptr<double>>::iterator, bool> inserted;
@@ -281,7 +223,10 @@ Equation::Equation(std::list<string> rpn)
             // push onto the stack so that we can get it later
             stack.push_back(
                         [lhs, rhs, tok](){
-                            return BINARY[tok](lhs(), rhs()); 
+                            double lhsv = lhs();
+                            double rhsv = rhs();
+                            cerr << lhsv << tok << rhsv << endl;
+                            return BINARY[tok](lhsv, rhsv); 
                         });
 
         } else if(UNARY.count(tok)) {
@@ -296,12 +241,24 @@ Equation::Equation(std::list<string> rpn)
             // push onto the stack so that we can get it later
             stack.push_back(
                         [lhs, tok](){
-                            return UNARY[tok](lhs()); 
+                            double lhsv = lhs();
+                            cerr << tok << lhsv << endl;
+                            return UNARY[tok](lhsv); 
                         });
             
         } else {
             function<double()> foo;
-            if(args.count(tok) > 0) {
+            char* end = NULL;
+            double v = strtod(tok.c_str(), &end);
+            if((end - tok.c_str()) == (int)tok.size()) {
+                // number 
+                cerr << "const tok=" << v << endl;
+                foo = [v, tok]() 
+                { 
+                    cerr << "const tok " << tok << " " << v << endl;
+                    return v; 
+                };
+            } else if(args.count(tok) > 0) {
                 // bind this
                 auto tmp = args[tok];
                 cerr << "tok=" << tmp << endl;
@@ -309,15 +266,6 @@ Equation::Equation(std::list<string> rpn)
                 { 
                     cerr << tok << " " << tmp << " " << *tmp << endl;
                     return *tmp; 
-                };
-            } else if(isnumeric(tok)) {
-                // 
-                double tmp = atof(tok.c_str());
-                cerr << "const tok=" << tmp << endl;
-                foo = [tmp, tok]() 
-                { 
-                    cerr << "const tok " << tok << " " << tmp << endl;
-                    return tmp; 
                 };
             } else {
                 // need to create it
@@ -338,60 +286,54 @@ Equation::Equation(std::list<string> rpn)
     cerr << stack.back()() << endl;
 }
 
-void explainRegexError(std::regex_error& e)
+list<string> tokenize(string exp)
 {
-    switch(e.code()) {
-	case std::regex_constants::error_collate:
-        cerr << "The expression contained an invalid collating element name."
-            << endl;
-		break;
-	case std::regex_constants::error_ctype:
-        cerr << "The expression contained an invalid character class name." 
-            << endl;
-		break;
-	case std::regex_constants::error_escape:
-        cerr << "The expression contained an invalid escaped character, or a "
-            "trailing escape." << endl;
-		break;
-	case std::regex_constants::error_backref:
-        cerr << "The expression contained an invalid back reference." << endl;
-		break;
-	case std::regex_constants::error_brack:
-        cerr << "The expression contained mismatched brackets ([ and ])." 
-            << endl;
-		break;
-	case std::regex_constants::error_paren:
-        cerr << "The expression contained mismatched parentheses (( and ))." 
-            << endl;
-		break;
-	case std::regex_constants::error_brace:
-        cerr << "The expression contained mismatched braces ({ and })." 
-            << endl;
-		break;
-	case std::regex_constants::error_badbrace:
-        cerr << "The expression contained an invalid range between braces ({ "
-            "and })." << endl;
-		break;
-	case std::regex_constants::error_range:
-        cerr << "The expression contained an invalid character range." << endl;
-		break;
-	case std::regex_constants::error_space:
-        cerr << "There was insufficient memory to convert the expression into "
-            "a finite state machine." << endl;
-		break;
-	case std::regex_constants::error_badrepeat:
-        cerr << "The expression contained a repeat specifier (one of *?+{) "
-            "that was not preceded by a valid regular expression." << endl;
-		break;
-	case std::regex_constants::error_complexity:
-        cerr << "The complexity of an attempted match against a regular "
-            "expression exceeded a pre-set level." << endl;
-		break;
-	case std::regex_constants::error_stack:
-        cerr << "There was insufficient memory to determine whether the "
-            "regular expression could match the specified character sequence."
-            << endl;
-		break;
-    }
-}
+    cerr << "Equation: " << exp << endl;
+    bool restart = true; // restart loop
+    list<string> out;
+    string singlechar = " ";
+    for(size_t ii=0; ii<exp.size();) {
+        restart = false;
+        while(ii < exp.length() && isspace(exp[ii]))
+            ii++;
 
+        if(exp[ii] == ')' || exp[ii] == '(') {
+            singlechar[0] = exp[ii];
+            out.push_back(singlechar);
+            ii++;
+            continue;
+        }
+
+        // check for operators
+        for(auto& v : PRIORITY) {
+            if(exp.compare(ii, v.first.length(), v.first) == 0) {
+                out.push_back(v.first);
+                ii += v.first.length();
+                restart = true;
+                break;
+            }
+        }
+        if(restart) continue;
+
+        // check for char
+        if(isalpha(exp[ii])) {
+            singlechar[0] = exp[ii];
+            out.push_back(singlechar);
+            ii++;
+            continue;
+        }
+
+        // otherwise assume its a number
+        char* end;
+        strtod(&exp.c_str()[ii], &end);
+        if(end == &exp.c_str()[ii]) {
+            throw INVALID_ARGUMENT("Unknown character: "+exp[ii]);
+        } else {
+            size_t len = ((end-&exp.c_str()[ii]));
+            out.push_back(exp.substr(ii, len));
+            ii += len;
+        }
+    }
+
+    return out;
+}
